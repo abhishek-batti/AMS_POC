1c95389d53d8d689555db060738d2238ab7232f6
abhishek-batti <ab.incture@gmail.com>
2025-09-08 05:20:56 -0700

    Added functionality for categorization

--- Query_input_page.py
+++ Query_input_page.py
@@ -1,5 +1,17 @@
 import streamlit as st
 from classes.ticket import Ticket
+import joblib
+import json
+
+def classify_category(description: str) -> str:
+    loaded_model = joblib.load("models\svm_tfidf_pipeline.pkl")
+    preds = loaded_model.predict([description])
+    with open("mappings\\label_mappings.json") as f:
+        mappings = json.load(f)
+    id2label_loaded = mappings["id2label"]
+    preds = loaded_model.predict([description])
+    class_preds = [id2label_loaded[str(p)] for p in preds]
+    return class_preds[0].split("/")
 
 def query_input_page():
     with st.container() as con:
@@ -12,7 +24,12 @@ def query_input_page():
             submitted = st.form_submit_button("Submit")
             if submitted and user_query.strip():
                 ticket = Ticket(description=user_query)
+                classification = classify_category(user_query)
+                st.write(f"Predicted Category: {classification}")
+                ticket.category = classification[0]
+                ticket.sub_category = classification[1]
                 st.success("Ticket created successfully!")
                 st.session_state["ticket"] = ticket
+                
 if __name__ == "__main__":
     query_input_page()
